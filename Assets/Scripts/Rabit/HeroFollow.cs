d2084abc91601ef0cf4db34f49a3991b4ffddf97
Maxim <maxim4110@gmail.com>
2018-05-20 19:28:49 +0300

    First commit

---
+++ Assets/Scripts/Rabit/HeroFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroFollow : MonoBehaviour {
+
+	public HeroRabit rabit;
+
+	void Start()
+	{
+		
+	}
+
+	void Update () 
+	{
+		Vector3 new_camera_position = transform.position;
+		new_camera_position.x = rabit.transform.position.x;
+		new_camera_position.y = rabit.transform.position.y;
+		transform.position = new_camera_position;
+	}
+}
---
+++ Assets/Scripts/Rabit/HeroRabit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRabit : MonoBehaviour
+{
+	public float speed = 1;
+	private Rigidbody2D myBody;
+	private SpriteRenderer sprite;
+	
+	void Start () {
+		myBody = GetComponent<Rigidbody2D> ();
+		sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	void FixedUpdate () {
+		float value = Input.GetAxis ("Horizontal");
+		if (Mathf.Abs (value) > 0) {
+			Vector2 vel = myBody.velocity;
+			vel.x = value * speed;
+			myBody.velocity = vel;
+		}
+		
+		if(value < 0) {
+			sprite.flipX = true;
+		} else if(value > 0) {
+			sprite.flipX = false;
+		}
+	}
+	
+	void Update () {
+		
+	}
+}
